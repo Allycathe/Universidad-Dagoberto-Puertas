72f18d06649065756b91db08f3522325694646f1
Bladeplan <alonso.iturra@mail.udp.cl>
2024-11-28 01:22:43 -0300

    Sistema de administración de la base de datos de la Universidad Dagoberto Puertas

---
+++ db/neon.js
@@ -0,0 +1,3 @@
+import { neon } from '@neondatabase/serverless';
+const sql= neon('postgresql://neondb_owner:iBES9KmkAZ2N@ep-fragrant-meadow-a8fa4vvx.eastus2.azure.neon.tech/neondb?sslmode=require');
+export default sql;
