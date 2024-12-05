e5a77e9176613c05ae2d5cb9eb760b42a418b702
Allycathe <alonso.iturra.j@gmail.com>
2024-12-05 18:23:15 -0300

    Endpoints de cada entidad terminados

--- routes/recursos.js
+++ routes/recursos.js
@@ -7,3 +7,24 @@ recursosRouter.get('/recursos', async (req, res)=>{
     const lista = await sql('select * from Recursos');
     res.render('recursos', {lista});
 });
+recursosRouter.post('/recursos/insertar', (req,res)=>{
+    const codigo=req.body.codigo;
+    const copias=req.body.copias;
+    const nombre=req.body.nombre;
+    const autor=req.body.autor;
+    const fecha=req.body.fecha;
+    const formato=req.body.formato;
+    const edifico=req.body.edificio;
+    sql('')
+})
+recursosRouter.post('/recursos/editar/:id',  (req,res)=>{
+    const codigo=req.body.codigo;
+    const disponibilidad=req.body.disponibilidad;
+    const copias=req.body.copias;
+    const disponible=req.body.disponiblel
+    const nombre=req.body.nombre;
+    const autor=req.body.autor;
+    const fecha=req.body.fecha;
+    const formato=req.body.formato;
+    const edifico=req.body.edificio;
+})
