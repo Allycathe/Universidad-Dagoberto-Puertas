c7160b78c35880fb3a70c5845d8e37a64df57c43
Bladeplan <alonso.iturra@mail.udp.cl>
2024-12-05 00:45:22 -0300

    .env funcional para tener el repo público y no exponer la bd

--- db/neon.js
+++ db/neon.js
@@ -2,6 +2,6 @@ import { neon } from '@neondatabase/serverless';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const sql= neon('postgresql://neondb_owner:iBES9KmkAZ2N@ep-fragrant-meadow-a8fa4vvx.eastus2.azure.neon.tech/neondb?sslmode=require');
+const sql= neon(process.env.DATABASE_URL);
 
 export default sql;
